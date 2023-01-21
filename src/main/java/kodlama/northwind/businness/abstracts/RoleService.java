e4713ad404a3f0c4e3be9161425cdfb4a3f6011f
mkucukkoc <1234567mustafa.mkk@gmail.com>
2023-01-21 12:19:11 +0300

    Authorize düzenlendi

--- src/main/java/kodlama/northwind/businness/abstracts/RoleService.java
+++
@@ -1,9 +0,0 @@
-package kodlama.northwind.businness.abstracts;
-
-import kodlama.northwind.core.entities.Role;
-import kodlama.northwind.core.utilities.results.Result;
-
-public interface RoleService {
-
-	//Result add(Role role);
-}
